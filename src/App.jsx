e8273c8f1a3329f23e2a3eb7dd4ebc77f835b294
Will Fay <williamfay08@gmail.com>
2022-11-01 19:48:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+const [burgerQuantity, setBurgerQuantity] = useState('');
+const [friesQuantity, setFriesQuantity] = useState('');
+const [shakeQuantity, setShakeQuantity] = useState('');
+const [saladQuantity, setSaladQuantity] = useState('');
+
+function handleChange(event, updater) {
+  updater(event.target.value)
+}
+
+function price(food) {
+  if (food === 'burger') {
+    '$'+(5 * parseInt(burgerQuantity)).toFixed(2)
+  } if (food === 'fries') {
+    '$'+(3.5 * parseInt(friesQuantity)).toFixed(2)
+  } if (food === 'shake') {
+    '$'+(2.75 * parseInt(shakerQuantity)).toFixed(2)
+  } if (food === 'salad') {
+    '$'+(4.75 * parseInt(saladQuantity)).toFixed(2)
+  }
+}
 
   return (
     <div className="App">
@@ -15,7 +35,8 @@ function App() {
               id="item-burger"
               type="text"
               placeholder="How many hamburgers"
-              value="0"
+              value={ burgerQuantity }
+              onChange={ (event) =>  handleChange(event, setBurgerQuantity)}
             ></input>
           </section>
           <section>
@@ -24,7 +45,8 @@ function App() {
               id="item-fries"
               type="text"
               placeholder="How many fries"
-              value="0"
+              value={ friesQuantity }
+              onChange={ (event) =>  handleChange(event, setFriesQuantity)}
             ></input>
           </section>
           <section>
@@ -33,7 +55,8 @@ function App() {
               id="item-shakes"
               type="text"
               placeholder="How many shakes"
-              value="0"
+              value={ shakeQuantity }
+              onChange={ (event) =>  handleChange(event, setShakeQuantity)}
             ></input>
           </section>
           <section>
@@ -42,7 +65,8 @@ function App() {
               id="item-salads"
               type="text"
               placeholder="How many salads"
-              value="0"
+              value={ saladQuantity }
+              onChange={ (event) =>  handleChange(event, setSaladQuantity)}
             ></input>
           </section>
           <input id="submit-order" type="submit" />
@@ -52,6 +76,13 @@ function App() {
         <h2>Order Subtotal:</h2>
         <p class="order-subtotal"></p>
         <ol className="items-list"></ol>
+         { burgerQuantity ?
+          <li>Burger Quantity: {burgerQuantity} - Price: ${(5 * parseInt(burgerQuantity)).toFixed(2)}</li>
+          : <li>Burger Quantity: None</li>
+         }
+          <li>Fries Quantity: {friesQuantity} - Price: ${(3.5 * parseInt(friesQuantity)).toFixed(2)}</li>
+          <li>Shake Quantity: {shakeQuantity} - Price: ${(2.75 * parseInt(shakeQuantity)).toFixed(2)}</li>
+          <li>Salad Quantity: {saladQuantity} - Price: ${(4.75 * parseInt(friesQuantity)).toFixed(2)}</li>
       </section>
     </div>
   );
